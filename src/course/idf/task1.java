a7d90c2387cb2e8029383d0e46210805c538a564
git_admin <sidorkina.oksana.19@gmail.com>
2020-03-30 22:01:53 +0000

    added tesk desc

--- src/course/idf/task1.java
+++ src/course/idf/task1.java
@@ -2,6 +2,18 @@ package course.idf;
 
 public class task1 {
     public static void main(String[] args) {
+        /* convert numbers
+        K (dec) = ... (bin) = ... (hex),
+        L (hex) = ... (bin) = ... (dec),
+        M (bin) = ... (hex) = ... (dec).
+        where
+            K (dec) L (hex) M (bin)
+        01 3009     2AB     0011 0110 0000
+        02 3555     4BA     0011 1101 1101
+        03 2996     27E     0011 1100 1111
+        04 4050     35B     0011 1011 1011
+        05 3120     2B7     0010 0111 1001
+        */
         System.out.println("0b"+decimalToBinary(123));
 
     }
