70833acc30f820edf980f88eb33b8dac8c5828c6
git_admin <sidorkina.oksana.19@gmail.com>
2020-03-31 23:37:10 +0000

    buildChristmasTreeElement

--- src/course/idf/task_3.java
+++ src/course/idf/task_3.java
@@ -22,17 +22,37 @@ public class task_3 {
         printFancyHeader("Lesson 3. Task 2. Type 4"); // using triangles type 4 + type 2
         buildTriangle(4, defaultDepth);
         buildTriangle(2, defaultDepth);
+        printFancyHeader("Lesson 3. Task 3. Build "); // using triangles type 4 + type 2
+        int baseWidth = 12, increaseWidth = 4, maxRows = 4, maxFrameWidth = baseWidth + maxRows * increaseWidth;
+        String[] rowFillers = {"*", "$", "%", "@"};
+        for (int i = 0; i < maxRows; i++)
+            buildChristmasTreeElement(baseWidth + i * increaseWidth, rowFillers[i], maxFrameWidth);
     }
 
-    public static void buildTriangle(int mode, int depth) {
+    public static void buildChristmasTreeElement(int maxWidth, String rowFiller, int maxFrameWidth) {
+        String space = " ";
+        int evenTop = (maxWidth % 2 == 0) ? 2 : 1;
+        int rowsToBuild = maxWidth / 2 + ((maxWidth % 2 == 0) ? 0 : 1);  // uneven width needs one more row
+        int padding = (maxFrameWidth - maxWidth) / 2;  // in case we build a tree within pre-defined frame
+        for (int i = 0; i < rowsToBuild; i++) {
+            System.out.println(space.repeat(padding + rowsToBuild - 1 - i) + rowFiller.repeat(evenTop + i * 2) + space.repeat(padding + rowsToBuild - 1 - i));
+        }
+    }
+
+    public static void buildChristmasTreeElement(int maxWidth, String rowFiller) {
+        buildChristmasTreeElement(maxWidth, rowFiller, maxWidth);
+    }
+
+    public static void buildTriangle(int mode, int maxWidth) {
         /* mode 1 - left-up;    mode 2 - left-down
            mode 3 - right-down; mode 4 - right-up */
         String rowFiller = ".", space = " ";
-        for (int i = 0; i < depth; i++)
+        for (int i = 0; i < maxWidth; i++)
             if (mode == 1) System.out.println(rowFiller.repeat(i + 1));  // left-up
-            else if (mode == 2) System.out.println(rowFiller.repeat(depth - i) + space.repeat(i)); //left-down
-            else if (mode == 3) System.out.println(space.repeat(i) + rowFiller.repeat(depth - i)); // right-down
-            else if (mode == 4) System.out.println(space.repeat(depth - i - 1) + rowFiller.repeat(i + 1)); // right-up
+            else if (mode == 2) System.out.println(rowFiller.repeat(maxWidth - i) + space.repeat(i)); //left-down
+            else if (mode == 3) System.out.println(space.repeat(i) + rowFiller.repeat(maxWidth - i)); // right-down
+            else if (mode == 4)
+                System.out.println(space.repeat(maxWidth - i - 1) + rowFiller.repeat(i + 1)); // right-up
     }
 
     public static void buildTriangle(int mode) {
